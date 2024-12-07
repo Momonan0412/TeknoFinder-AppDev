88e404bbd95d0cb831e1f62ecb3b5bb8f38faab9
codedumpforArmino <73728192+codedumpforArmino@users.noreply.github.com>
2024-12-07 16:09:45 +0800

    map test

--- TeknoFinder-AppDev-Backend/App-Dev-API/Program.cs
+++ TeknoFinder-AppDev-Backend/App-Dev-API/Program.cs
@@ -14,7 +14,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AppDevCorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
+
+app.UseCors("AppDevCorsPolicy");
 
 
 // Configure the HTTP request pipeline.
--- TeknoFinder-AppDev-Backend/App-Dev-API/obj/Debug/net8.0/AppDev.API.AssemblyInfo.cs
+++ TeknoFinder-AppDev-Backend/App-Dev-API/obj/Debug/net8.0/AppDev.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppDev.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573f28848098e0934511ceafa5fa75bf74047027")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppDev.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppDev.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
