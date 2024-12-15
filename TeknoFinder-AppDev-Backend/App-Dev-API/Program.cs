110edb8f8f3191e8a3dcbffb2305e4d727b01a80
meepmeep <elijahsabay@gmail.com>
2024-12-15 12:15:10 +0800

    bye2 fake confession and dis now working

--- TeknoFinder-AppDev-Backend/App-Dev-API/Program.cs
+++ TeknoFinder-AppDev-Backend/App-Dev-API/Program.cs
@@ -95,11 +95,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors("AppDevCorsPolicy");
-<<<<<<< HEAD
-=======
 
-
->>>>>>> origin/initial-templates
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
