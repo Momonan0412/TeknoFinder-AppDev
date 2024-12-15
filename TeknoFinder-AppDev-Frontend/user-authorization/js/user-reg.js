af3f4053490e3059690843b31be4adef6c01bd7e
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-12-15 13:19:07 +0800

    added registration fuctionality

--- TeknoFinder-AppDev-Frontend/user-authorization/js/user-reg.js
+++ TeknoFinder-AppDev-Frontend/user-authorization/js/user-reg.js
@@ -16,26 +16,36 @@ $(document).ready(function () {
     "isActive": true
   }
 }
-
     if(sessionStorage.getItem("userDetails") == null) {
         sessionStorage.setItem("userDetails", JSON.stringify(tempUserDetails));
     }
+    
     const userDetails = JSON.parse(sessionStorage.getItem("userDetails"));
+    console.log(userDetails);
     console.log(window.location.href);
-    $('.btn-next').click(function() {
+    // I know this is disgusting, please dont judge me I dont have a lot of time
+    $('#reg-one').click(function() {
         console.log("redirecting....")
         userDetails.studentDTO.firstName = $('#firstname').val();
         userDetails.studentDTO.lastName = $('#lastname').val();
+        userDetails.addUserDTO.username = $('#username').val();
+        userDetails.studentDTO.yearLevel = $('#yearlevel').val();
+        userDetails.studentDTO.program = $('#program').val();
         console.log(userDetails);
         sessionStorage.setItem("userDetails", JSON.stringify(userDetails));
         window.location.href = "register-student-two.html";
     });
-    $('.btn-prev').click(function() {
-        userDetails.studentDTO.studentNumber = $('').val();
-        userDetails.addUserDTO.password = $('').val();
-        userDetails.addUserDTO.confirmPassword = $('').val();
+    $('#reg-two').click(function() {
+        userDetails.studentDTO.studentNumber = $('#studentnumber').val();
+        userDetails.studentDTO.status = $('#studentstatus').val();
+        userDetails.addUserDTO.email = $('#studentemail').val();
+        userDetails.addUserDTO.password = $('#password').val();
+        userDetails.addUserDTO.confirmPassword = $('#confirmpassword').val();
         console.log(userDetails);
         sessionStorage.setItem("userDetails", JSON.stringify(userDetails));
-        window.location.href = "register-student-two.html";
+        sessionStorage.removeItem("userDetails");
+        $.ajax({
+          
+        })
     });
 });
